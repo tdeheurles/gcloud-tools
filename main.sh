2287f1fe2614762bcbbf97b4213e445940b9ee77
tdeheurles <tdeheurles@gmail.com>
2015-06-22 12:06:31 +0200

    Try to correct \r issue

--- main.sh
+++ main.sh
@@ -7,11 +7,11 @@ artifact_tag="$servicemajor.$serviceminor.$BUILD_NUMBER"
 artifact_name="gcr.io/$projectid/$servicename"
 artifact_name_tagged="$artifact_name:$artifact_tag"
 
-  # Build
+# Build
 docker build -t $artifact_name .
 docker tag $artifact_name $artifact_name_tagged
 
-  # Push to Google Cloud Engine
+# Push to Google Cloud Engine
 gcloud preview docker push $artifact_name
 gcloud preview docker push $artifact_name_tagged
 
@@ -20,10 +20,10 @@ gcloud preview docker push $artifact_name_tagged
 public_artifact_name="$public_projectid/$servicename"
 public_artifact_name_tagged="$public_artifact_name:$artifact_tag"
 
-  # tag
+# tag
 docker tag -t $artifact_name $public_artifact_name
 docker tag -t $artifact_name $public_artifact_name_tagged
 
-  # pull
+# pull
 docker push $public_artifact_name
 docker push $public_artifact_name_tagged
