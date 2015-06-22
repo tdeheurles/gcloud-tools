0a1c5cf9ff310b0a521cc6afb57d0f6290305a4c
tdeheurles <tdeheurles@gmail.com>
2015-06-22 11:52:28 +0200

    corrected intend for script

--- main.sh
+++ main.sh
@@ -4,26 +4,26 @@
 artifact_tag="$servicemajor.$serviceminor.$BUILD_NUMBER"
 
 # PRIVATE
-  artifact_name="gcr.io/$projectid/$servicename"
-  artifact_name_tagged="$artifact_name:$artifact_tag"
+artifact_name="gcr.io/$projectid/$servicename"
+artifact_name_tagged="$artifact_name:$artifact_tag"
 
   # Build
-  docker build -t $artifact_name .
-  docker tag $artifact_name $artifact_name_tagged
+docker build -t $artifact_name .
+docker tag $artifact_name $artifact_name_tagged
 
   # Push to Google Cloud Engine
-  gcloud preview docker push $artifact_name
-  gcloud preview docker push $artifact_name_tagged
+gcloud preview docker push $artifact_name
+gcloud preview docker push $artifact_name_tagged
 
 
 # PUBLIC
-  public_artifact_name="$public_projectid/$servicename"
-  public_artifact_name_tagged="$public_artifact_name:$artifact_tag"
+public_artifact_name="$public_projectid/$servicename"
+public_artifact_name_tagged="$public_artifact_name:$artifact_tag"
 
   # tag
-  docker tag -t $artifact_name $public_artifact_name
-  docker tag -t $artifact_name $public_artifact_name_tagged
+docker tag -t $artifact_name $public_artifact_name
+docker tag -t $artifact_name $public_artifact_name_tagged
 
   # pull
-  docker push $public_artifact_name
-  docker push $public_artifact_name_tagged
+docker push $public_artifact_name
+docker push $public_artifact_name_tagged
