4f3e80b33092fccfc84c586779587905c450802c
tdeheurles <tdeheurles@gmail.com>
2015-06-22 11:48:53 +0200

    Added push to public repository

--- main.sh
+++ main.sh
@@ -1,12 +1,29 @@
 # import names
 . ./release.cfg
-artifact_name="gcr.io/$projectid/$servicename"
-artifact_tag="$artifact_name:$servicemajor.$serviceminor.$BUILD_NUMBER"
 
-# Build
-docker build -t $artifact_name .
-docker tag $artifact_name $artifact_tag
+artifact_tag="$servicemajor.$serviceminor.$BUILD_NUMBER"
 
-# Push to Google Cloud Engine
-gcloud preview docker push $artifact_name
-gcloud preview docker push $artifact_tag
+# PRIVATE
+  artifact_name="gcr.io/$projectid/$servicename"
+  artifact_name_tagged="$artifact_name:$artifact_tag"
+
+  # Build
+  docker build -t $artifact_name .
+  docker tag $artifact_name $artifact_name_tagged
+
+  # Push to Google Cloud Engine
+  gcloud preview docker push $artifact_name
+  gcloud preview docker push $artifact_name_tagged
+
+
+# PUBLIC
+  public_artifact_name="$public_projectid/$servicename"
+  public_artifact_name_tagged="$public_artifact_name:$artifact_tag"
+
+  # tag
+  docker tag -t $artifact_name $public_artifact_name
+  docker tag -t $artifact_name $public_artifact_name_tagged
+
+  # pull
+  docker push $public_artifact_name
+  docker push $public_artifact_name_tagged
