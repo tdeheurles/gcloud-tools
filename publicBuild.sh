f7fa4364413d2110978b7bde47c1a67426ebaee4
tdeheurles <tdeheurles@gmail.com>
2015-06-28 20:32:36 +0000

    CRLF -> LF

--- publicBuild.sh
+++ publicBuild.sh
@@ -9,7 +9,7 @@ public_artifact_name="$public_projectid/$servicename"
 public_artifact_name_tagged="$public_artifact_name:$artifact_tag"
 
 # Build
-docker build -t $public_artifact_name .
+docker build --no-cache -t $public_artifact_name .
 docker tag $public_artifact_name $public_artifact_name_tagged
 
 # push
