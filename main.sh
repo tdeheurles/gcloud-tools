69379a2c5fa710f18b0ca8b0b12b70d7b0995e77
tdeheurles <tdeheurles@gmail.com>
2015-07-01 11:33:57 +0200

    Removed cache from docker build

--- main.sh
+++ main.sh
@@ -9,7 +9,7 @@ artifact_name="gcr.io/$projectid/$servicename"
 artifact_name_tagged="$artifact_name:$artifact_tag"
 
 # Build
-docker build -t $artifact_name .
+docker build --no-cache -t $artifact_name .
 docker tag $artifact_name $artifact_name_tagged
 
 # Push to Google Cloud Engine
